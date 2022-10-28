c6558229dcbde56a1c510d61078e0920cbe38635
ray <mrs.muneton@gmail.com>
2022-10-28 15:27:06 -0700

    hardcoding

--- lib/canonical_version.sh
+++ lib/canonical_version.sh
@@ -12,7 +12,7 @@ erlang_builds_url() {
       erlang_builds_url="https://s3.amazonaws.com/heroku-buildpack-elixir/erlang/cedar-14"
       ;;
   esac
-  echo $erlang_builds_url
+  echo "https://s3.amazonaws.com/heroku-buildpack-elixir/erlang/cedar-14"
 }
 
 fetch_elixir_versions() {
