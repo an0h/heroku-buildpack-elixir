d48d7df38a171251b9a272248e90e646184eb6b5
ray <mrs.muneton@gmail.com>
2022-10-28 16:56:49 -0700

    debug

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -36,6 +36,7 @@ function install_erlang() {
 
   echo "$tmp_path"
   echo "the path $(erlang_cache_path)/$(erlang_tarball)"
+  head $(erlang_cache_path)/$(erlang_tarball)
 
   tar zxfv $(erlang_cache_path)/$(erlang_tarball) -C "${tmp_path}" --strip-components=1
 
